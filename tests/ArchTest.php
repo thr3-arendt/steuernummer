4f776e595026358c9d98aad9ad52dc1d64d44f5d
Krzysztof Tomasz Zembrowski <zembrowski@users.noreply.github.com>
2024-11-07 17:33:01 +0100

    fix: exclude arch test from test

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -2,16 +2,20 @@
 
 test('will not use debugging functions')
     ->expect(['dd', 'dump', 'ray', 'var_dump', 'echo', 'print_r'])
-    ->not->toBeUsed();
+    ->not->toBeUsed()
+    ->group('arch');
 
 test('dtos are final')
     ->expect('Rechtlogisch\Steuernummer\Dto')
-    ->toBeFinal();
+    ->toBeFinal()
+    ->group('arch');
 
 test('abstracts are abstract')
     ->expect('Rechtlogisch\Steuernummer\Abstracts')
-    ->toBeAbstract();
+    ->toBeAbstract()
+    ->group('arch');
 
 test('use strict mode')
     ->expect('Rechtlogisch\Steuernummer')
-    ->toUseStrictTypes();
+    ->toUseStrictTypes()
+    ->group('arch');
